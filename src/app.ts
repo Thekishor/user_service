b5d56687e09d44ed73fae16b5783fddbbae53710
Kis hor <141760112+Thekishor@users.noreply.github.com>
2026-08-11 16:29:30 +0545

    cors, compression setup

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,19 @@ import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes";
 import { errorHandler } from "./middleware/error.handler";
 import adminRouter from "./routes/admin.routes";
+import compression from "compression";
+import cors from "cors";
+import { env } from "./config/env";
 
 const app = express();
 app.disable("x-powered-by");
 
+app.use(cors({
+    origin: env.FRONTEND_URL,
+    credentials: true,
+}));
+
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- src/config/env.ts
+++ src/config/env.ts
@@ -8,10 +8,12 @@ const envSchema = z.object({
     NODE_ENV: z.string(),
     MONGO_URI: z.string(),
     JWT_ACCESS_SECRET: z.string(),
+    JWT_ACCESS_EXPIRY: z.string(),
     JWT_REFRESH_SECRET: z.string(),
     COOKIE_REFRESH_MAX_AGE: z.string().transform(Number),
     RESEND_API_KEY: z.string(),
     APP_URL: z.string(),
+    FRONTEND_URL: z.string(),
 })
 
 export const env = envSchema.parse(process.env);
