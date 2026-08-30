d786cb6e9162f6550ea4301779a304c03d11e9af
Kis hor <141760112+Thekishor@users.noreply.github.com>
2026-08-30 12:29:11 +0545

    redis config

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -13,6 +13,7 @@ const envSchema = z.object({
     RESEND_API_KEY: z.string(),
     APP_URL: z.string(),
     FRONTEND_URL: z.string(),
+    REDIS_URL: z.string(),
     DOMAIN: z.string(),
 })
 
---
+++ backend/src/config/redis.config.ts
@@ -0,0 +1,55 @@
+import { createClient } from "redis";
+import logger, { logError } from "./logger.js";
+import { env } from "./env";
+
+export const redis = createClient({
+    url: env.REDIS_URL,
+    // If Redis is disconnected, commands aren't queued in Node.js waiting for Redis to come back.
+    disableOfflineQueue: true,
+    socket: {
+        connectTimeout: 10000,
+        reconnectStrategy: (retries) => {
+            return Math.min(retries * 200, 5000);
+        }
+    }
+});
+
+redis.on("connect", () => {
+    logger.info("Redis connected...");
+})
+
+redis.on("error", (err) => {
+    logError("Redis error...", err);
+});
+
+redis.on("ready", () => {
+    logger.info("Redis is ready...");
+});
+
+redis.on("reconnecting", () => {
+    logger.warn("Redis reconnecting...");
+});
+
+redis.on("end", () => {
+    logger.warn("Redis connection ended...");
+});
+
+export const connectRedis = async () => {
+    try {
+        await redis.connect();
+        logger.info("Redis connected successfully");
+    } catch (error) {
+       logError("Redis failed to connect...", error);
+    }
+}
+
+export const disconnectRedis = async () => {
+    try {
+        // graceful shutdown
+        await redis.quit();
+        logger.info("Redis disconnected gracefully");
+    } catch (error) {
+        logError("Graceful Redis exit failed, forcing socket destruction...", error);
+        redis.destroy();
+    }
+}
